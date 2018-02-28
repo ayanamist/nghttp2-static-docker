7c861eb31c2d567d5710cae5153fd2551dba9c92
ayanamist <ayanamist@gmail.com>
2018-02-28 13:59:48 +0800

    bump to 1.31.0, use multi-stage build

--- build.sh
+++ build.sh
@@ -5,62 +5,30 @@ if [[ -z ${MAKE_J} ]]; then
   MAKE_J=$(grep -c ^processor /proc/cpuinfo)
 fi
 
-ZLIB_VER=1.2.11
-ZLIB_URL=http://zlib.net/zlib-${ZLIB_VER}.tar.gz
-ZLIB_SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
-
-LIBEV_VER=4.24
-LIBEV_URL=http://dist.schmorp.de/libev/Attic/libev-${LIBEV_VER}.tar.gz
-LIBEV_SHA256=973593d3479abdf657674a55afe5f78624b0e440614e2b8cb3a07f16d4d7f821
-
-OPENSSL_VER=1_0_2n
-OPENSSL_URL=https://github.com/openssl/openssl/archive/OpenSSL_${OPENSSL_VER}.tar.gz
-OPENSSL_SHA256=4f4bc907caff1fee6ff8593729e5729891adcee412049153a3bb4db7625e8364
-
-NGHTTP2_VER=1.29.0
-NGHTTP2_URL=https://github.com/nghttp2/nghttp2/releases/download/v${NGHTTP2_VER}/nghttp2-${NGHTTP2_VER}.tar.gz
-NGHTTP2_SHA256=e75cc2be3aa7a8390df615c27ebafaa5900f00e63b454b05c477d131de813548
-
-C_ARES_VER=1.13.0
-C_ARES_URL=https://c-ares.haxx.se/download/c-ares-${C_ARES_VER}.tar.gz
-C_ARES_SHA256=03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7
-
-WGET="wget --no-check-certificate --secure-protocol=TLSv1 -T 30 -nv"
-
 mkdir /build &&\
 cd /build &&\
-${WGET} -O zlib.tar.gz ${ZLIB_URL} &&\
-echo "$ZLIB_SHA256  zlib.tar.gz" | sha256sum -c - &&\
 tar xf zlib.tar.gz &&\
-cd zlib-${ZLIB_VER} &&\
+cd zlib-* &&\
 ./configure --static &&\
 make clean install -j${MAKE_J} &&\
 cd .. &&\
-${WGET} -O libev.tar.gz ${LIBEV_URL} &&\
-echo "${LIBEV_SHA256}  libev.tar.gz" | sha256sum -c - &&\
 tar xf libev.tar.gz &&\
-cd libev-${LIBEV_VER} &&\
+cd libev-* &&\
 ./configure --enable-static --disable-shared &&\
 make clean install -j${MAKE_J} &&\
 cd .. &&\
-${WGET} -O openssl.tar.gz ${OPENSSL_URL} &&\
-echo "${OPENSSL_SHA256}  openssl.tar.gz" | sha256sum -c - &&\
 tar xf openssl.tar.gz &&\
-cd openssl-OpenSSL_${OPENSSL_VER} &&\
+cd openssl-* &&\
 ./config zlib &&\
 make clean install &&\
 cd .. &&\
-${WGET} -O c-ares.tar.gz ${C_ARES_URL} &&\
-echo "${C_ARES_SHA256}  c-ares.tar.gz" | sha256sum -c - &&\
 tar xf c-ares.tar.gz &&\
-cd c-ares-${C_ARES_VER} &&\
+cd c-ares-* &&\
 ./configure --disable-dependency-tracking --enable-shared=no --enable-static=yes &&\
 make clean install &&\
 cd .. &&\
-${WGET} -O nghttp2.tar.gz ${NGHTTP2_URL} &&\
-echo "${NGHTTP2_SHA256}  nghttp2.tar.gz" | sha256sum -c - &&\
 tar xf nghttp2.tar.gz && \
-cd nghttp2-${NGHTTP2_VER} && \
+cd nghttp2-* && \
 export PKG_CONFIG_PATH="/usr/local/ssl/lib/pkgconfig:/usr/local/lib/pkgconfig" &&\
 export PKG_CONFIG="pkg-config --static" &&\
 export LDFLAGS="-static-libgcc -static-libstdc++ -static" &&\
---
+++ download.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+ZLIB_VER=1.2.11
+ZLIB_URL=http://zlib.net/zlib-${ZLIB_VER}.tar.gz
+ZLIB_SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
+
+LIBEV_VER=4.24
+LIBEV_URL=http://dist.schmorp.de/libev/Attic/libev-${LIBEV_VER}.tar.gz
+LIBEV_SHA256=973593d3479abdf657674a55afe5f78624b0e440614e2b8cb3a07f16d4d7f821
+
+OPENSSL_VER=1.0.2n
+OPENSSL_URL=https://www.openssl.org/source/openssl-${OPENSSL_VER}.tar.gz
+OPENSSL_SHA256=370babb75f278c39e0c50e8c4e7493bc0f18db6867478341a832a982fd15a8fe
+
+NGHTTP2_VER=1.31.0
+NGHTTP2_URL=https://github.com/nghttp2/nghttp2/releases/download/v${NGHTTP2_VER}/nghttp2-${NGHTTP2_VER}.tar.gz
+NGHTTP2_SHA256=6a2d02c441cf8d4279aea3c98d22763f8464808c3955db5c308291fe59d17cab
+
+C_ARES_VER=1.13.0
+C_ARES_URL=https://c-ares.haxx.se/download/c-ares-${C_ARES_VER}.tar.gz
+C_ARES_SHA256=03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7
+
+WGET="wget -T 30 -nv"
+
+apt-get install -y wget &&\
+mkdir /build &&\
+cd /build &&\
+${WGET} -O zlib.tar.gz ${ZLIB_URL} &&\
+echo "$ZLIB_SHA256  zlib.tar.gz" | sha256sum -c - &&\
+${WGET} -O libev.tar.gz ${LIBEV_URL} &&\
+echo "${LIBEV_SHA256}  libev.tar.gz" | sha256sum -c - &&\
+${WGET} -O openssl.tar.gz ${OPENSSL_URL} &&\
+echo "${OPENSSL_SHA256}  openssl.tar.gz" | sha256sum -c - &&\
+${WGET} -O c-ares.tar.gz ${C_ARES_URL} &&\
+echo "${C_ARES_SHA256}  c-ares.tar.gz" | sha256sum -c - &&\
+${WGET} -O nghttp2.tar.gz ${NGHTTP2_URL} &&\
+echo "${NGHTTP2_SHA256}  nghttp2.tar.gz" | sha256sum -c - &&\
