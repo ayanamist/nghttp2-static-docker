dfd89516da30754ec167f6f487e4689e802489cd
ayanamist <ayanamist@gmail.com>
2018-09-13 15:20:13 +0800

    Update openssl 1.1.1

--- download.sh
+++ download.sh
@@ -10,9 +10,9 @@ LIBEV_VER=4.24
 LIBEV_URL=http://dist.schmorp.de/libev/Attic/libev-${LIBEV_VER}.tar.gz
 LIBEV_SHA256=973593d3479abdf657674a55afe5f78624b0e440614e2b8cb3a07f16d4d7f821
 
-OPENSSL_VER=1.1.0i
+OPENSSL_VER=1.1.1
 OPENSSL_URL=https://www.openssl.org/source/openssl-${OPENSSL_VER}.tar.gz
-OPENSSL_SHA256=ebbfc844a8c8cc0ea5dc10b86c9ce97f401837f3fa08c17b2cdadc118253cf99
+OPENSSL_SHA256=2836875a0f89c03d0fdf483941512613a50cfb421d6fd94b9f41d7279d586a3d
 
 NGHTTP2_VER=1.33.0
 NGHTTP2_URL=https://github.com/nghttp2/nghttp2/releases/download/v${NGHTTP2_VER}/nghttp2-${NGHTTP2_VER}.tar.gz
