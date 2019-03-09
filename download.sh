d6ad2d10b73ec6541005a226fc70c744b227a303
ayanamist <ayanamist@gmail.com>
2019-03-09 18:52:01 +0800

    Bump to nghttp2 1.37.0 + openssl 1.1.1b

--- download.sh
+++ download.sh
@@ -8,11 +8,11 @@ ZLIB_SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
 LIBEV_URL=http://dist.schmorp.de/libev/Attic/libev-4.25.tar.gz
 LIBEV_SHA256=78757e1c27778d2f3795251d9fe09715d51ce0422416da4abb34af3929c02589
 
-OPENSSL_URL=https://www.openssl.org/source/openssl-1.1.1a.tar.gz
-OPENSSL_SHA256=fc20130f8b7cbd2fb918b2f14e2f429e109c31ddd0fb38fc5d71d9ffed3f9f41
+OPENSSL_URL=https://www.openssl.org/source/openssl-1.1.1b.tar.gz
+OPENSSL_SHA256=5c557b023230413dfb0756f3137a13e6d726838ccd1430888ad15bfb2b43ea4b
 
-NGHTTP2_URL=https://github.com/nghttp2/nghttp2/releases/download/v1.36.0/nghttp2-1.36.0.tar.gz
-NGHTTP2_SHA256=6b222a264aca23d497f7878a7751bd9da12676717493fe747db49afb51daae79
+NGHTTP2_URL=https://github.com/nghttp2/nghttp2/releases/download/v1.37.0/nghttp2-1.37.0.tar.gz
+NGHTTP2_SHA256=760981ab5703d3ed185eccb322321d379453974357a3263971a928c2879a43bf
 
 C_ARES_URL=https://github.com/c-ares/c-ares/releases/download/cares-1_15_0/c-ares-1.15.0.tar.gz
 C_ARES_SHA256=6cdb97871f2930530c97deb7cf5c8fa4be5a0b02c7cea6e7c7667672a39d6852
