ae938ba8be3c734daede349b0a3ccd7ad7e78b95
ayanamist <ayanamist@gmail.com>
2021-02-02 22:44:43 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,11 +8,11 @@ ZLIB_SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
 LIBEV_URL=http://dist.schmorp.de/libev/Attic/libev-4.33.tar.gz
 LIBEV_SHA256=507eb7b8d1015fbec5b935f34ebed15bf346bed04a11ab82b8eee848c4205aea
 
-OPENSSL_URL=https://www.openssl.org/source/openssl-1.1.1h.tar.gz
-OPENSSL_SHA256=5c9ca8774bd7b03e5784f26ae9e9e6d749c9da2438545077e6b3d755a06595d9
+OPENSSL_URL=https://www.openssl.org/source/openssl-1.1.1i.tar.gz
+OPENSSL_SHA256=e8be6a35fe41d10603c3cc635e93289ed00bf34b79671a3a4de64fcee00d5242
 
-NGHTTP2_URL=https://github.com/nghttp2/nghttp2/releases/download/v1.42.0/nghttp2-1.42.0.tar.gz
-NGHTTP2_SHA256=884d18a0158908125d58b1b61d475c0325e5a004e3d61a56b5fcc55d5f4b7af5
+NGHTTP2_URL=https://github.com/nghttp2/nghttp2/releases/download/v1.43.0/nghttp2-1.43.0.tar.gz
+NGHTTP2_SHA256=45cc3ed91966551f92b31958ceca9b3a9f23ce4faf5cbedb78aa3327cd4e5907
 
 C_ARES_URL=https://c-ares.haxx.se/download/c-ares-1.17.1.tar.gz
 C_ARES_SHA256=d73dd0f6de824afd407ce10750ea081af47eba52b8a6cb307d220131ad93fc40
